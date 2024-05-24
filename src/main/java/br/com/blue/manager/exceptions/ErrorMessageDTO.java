0294c000697457cf33b7287161b29470d59d1484
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-05-23 23:50:06 -0300

    Formatando mensagem de validação

---
+++ src/main/java/br/com/blue/manager/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,4 @@
+package br.com.blue.manager.exceptions;
+
+public record ErrorMessageDTO(String message, String field) {
+}
---
+++ src/main/java/br/com/blue/manager/exceptions/ExeceptionHandlerController.java
@@ -0,0 +1,35 @@
+package br.com.blue.manager.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+class ExeceptionHandlerController {
+  private MessageSource messageSource;
+
+  public ExeceptionHandlerController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    List<ErrorMessageDTO> errDTO = new ArrayList<>();
+
+    e.getBindingResult().getFieldErrors().forEach(err -> {
+      String message = this.messageSource.getMessage(err, LocaleContextHolder.getLocale());
+      ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
+      errDTO.add(error);
+    });
+
+    return new ResponseEntity<List<ErrorMessageDTO>>(errDTO, HttpStatus.OK);
+  }
+}
