dae1a8f935f0dfebbd885fcfc4ac0109f5ade19b
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-05-24 20:40:44 -0300

    criando repositório

---
+++ src/main/java/br/com/blue/manager/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package br.com.blue.manager.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+}
--- src/main/java/br/com/blue/manager/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/blue/manager/modules/candidate/controllers/CandidateController.java
@@ -1,20 +1,24 @@
 package br.com.blue.manager.modules.candidate.controllers;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.blue.manager.modules.candidate.CandidateEntity;
+import br.com.blue.manager.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-  @GetMapping("/")
-  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    return ResponseEntity.ok().body(candidateEntity);
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @GetMapping
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }
