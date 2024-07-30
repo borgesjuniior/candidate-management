54804e92edd1adfbcf9b48b41e4a37bd3297de3b
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-07-30 00:10:51 -0300

    Refatorando JobController

--- src/main/java/br/com/blue/manager/modules/company/controllers/JobController.java
+++ src/main/java/br/com/blue/manager/modules/company/controllers/JobController.java
@@ -1,5 +1,7 @@
 package br.com.blue.manager.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.blue.manager.modules.company.dto.CreateJobDTO;
 import br.com.blue.manager.modules.company.entities.JobEntity;
 import br.com.blue.manager.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,7 +23,16 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping
-  private ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+  private ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createjoDto, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    JobEntity jobEntity = JobEntity.builder()
+        .benefits(createjoDto.benefits())
+        .companyId(UUID.fromString(companyId.toString()))
+        .description(createjoDto.description())
+        .level(createjoDto.level())
+        .build();
+
     try {
       var result = this.createJobUseCase.execute(jobEntity);
       return ResponseEntity.ok().body(result);
---
+++ src/main/java/br/com/blue/manager/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,5 @@
+package br.com.blue.manager.modules.company.dto;
+
+public record CreateJobDTO(String description, String benefits, String level) {
+
+}
