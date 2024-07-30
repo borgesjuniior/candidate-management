b44963162e577f3924feedc7512ee531474758a9
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-07-29 23:32:08 -0300

    Validando Token

--- src/main/java/br/com/blue/manager/security/SecurityFilter.java
+++ src/main/java/br/com/blue/manager/security/SecurityFilter.java
@@ -1,10 +1,15 @@
 package br.com.blue.manager.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.blue.manager.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,12 +18,31 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+  @Autowired
+  private JWTProvider jwtProvider;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    String BearerToken = request.getHeader("Authorization");
+    SecurityContextHolder.getContext().setAuthentication(null);
+    String bearerToken = request.getHeader("Authorization");
 
-    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    if (bearerToken != null) {
+      var subjetToken = jwtProvider.validateToken(bearerToken);
+
+      if (subjetToken.isEmpty()) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+      }
+
+      request.setAttribute("company_id", subjetToken);
+
+      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjetToken, null,
+          Collections.emptyList());
+      SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    filterChain.doFilter(request, response);
   }
 
 }
