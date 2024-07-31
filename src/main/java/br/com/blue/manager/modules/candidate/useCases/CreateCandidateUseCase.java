26b8e420de7c82b96e13b1fcc9adb9b15a1615e8
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-07-30 23:05:04 -0300

    Hash da senha do candidato

--- src/main/java/br/com/blue/manager/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/blue/manager/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -3,6 +3,7 @@ package br.com.blue.manager.modules.candidate.useCases;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.blue.manager.exceptions.UserFoundException;
@@ -15,6 +16,9 @@ public class CreateCandidateUseCase {
   @Autowired
   private CandidateRepository candidateRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     Optional<CandidateEntity> candidate = this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
         candidateEntity.getEmail());
@@ -22,6 +26,9 @@ public class CreateCandidateUseCase {
     candidate.ifPresent(c -> {
       throw new UserFoundException("User already exists");
     });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
 
     return this.candidateRepository.save(candidateEntity);
   }
